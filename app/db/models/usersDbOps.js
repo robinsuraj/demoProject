b8a6ed508011b4de734320de0c5416f2e7758d50
Suraj <suraj.verma@skilrock.com>
2018-10-31 18:14:39 +0530

    second commit

--- app/db/models/usersDbOps.js
+++ app/db/models/usersDbOps.js
@@ -1,8 +1,6 @@
 'use strict'
 let UserData = require('../schemas/user');
-var jwt = require('jsonwebtoken');
-var config = require('../../../config/config');
-var httpStatus = require('http-status-codes');
+var nodemailer = require('nodemailer');
 
 
 
@@ -26,15 +24,12 @@ class userModel {
         }
         catch(err){
             console.log('--usersDbOps--',err);
-            return {err: true, msg:'Error while saving'} ;
+            return {err: true, msg:'Error .....'} ;
         }
     }
 
     async logInData(user){
         try{
-            var avoid = {
-                "password": 0
-            }
             const result = await UserData.findOne({
                 email:user.email,password: user.password });
             if(!result){
@@ -45,7 +40,7 @@ class userModel {
         }
         catch(err){
             console.log('--usersDbOps--',err);
-            return {err: true, msg:'Error while saving'} ;
+            return {err: true, msg:'Error....'} ;
         }
     }
 
@@ -57,14 +52,53 @@ class userModel {
                     return {err: true, msg:'Old password does not match'} ;
                 } else {
                     var password = user.newpass;
-                    const result = await User.findByIdAndUpdate({ _id: user.userId }, { $set: { password: password } }, { new: true });
+                    const result = await UserData.findByIdAndUpdate({ _id: user.userId }, { $set: { password: password } }, { new: true });
                     return result;
                 }
+            }else{
+                return {err: true, msg:'User does not exists'} ;
             }
         }
         catch(err){
             console.log('--usersDbOps--',err);
-            return {err: true, msg:'Error while saving'} ;
+            return {err: true, msg:'Error....'} ;
+        }
+    }
+
+    async forgetPassword(user){
+        try{
+            const result = await UserData.findOne({email: user.email});
+            if(result){
+                var transporter = nodemailer.createTransport({
+                    service: 'Gmail',
+                    auth: {
+                        user: "test.avi201@gmail.com",
+                        pass: "Mobiloitte1"
+                    }
+                });
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                var link = "";
+                for (var i = 0; i < 8; i++) link += possible.charAt(Math.floor(Math.random() * possible.length));
+                var to = user.email
+                var mailOption = {
+                    from: "test.avi201@gmail.com",
+                    to: user.email,
+                    subject: 'Demo Change Password ',
+                    text: 'you have a new submission with following details',
+                    html: "Your current Password is :" + link
+                }
+                const mailResult = await transporter.sendMail(mailOption);
+                if(mailResult){
+                    const finalResult = await UserData.findOneAndUpdate({ email: user.email }, {$set: {password: link }});
+                    return finalResult;
+                }
+            }else{
+                return {err: true, msg:'User does not exists'} ;
+            }
+        }
+        catch(err){
+            console.log('--usersDbOps--',err);
+            return {err: true, msg:'Error....'} ;
         }
     }
 }
